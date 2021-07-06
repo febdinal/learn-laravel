8173030c4425dbc77459ae3ad22d413f46937110
febdinal <febdinal08@gmail.com>
2021-07-06 11:38:12 +0700

    updateting view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <h1 style="text-align:center">Selamat</h1>
                 <h1 style="text-align:center">Berhasil Login</h1>
                 <a class="btn btn-danger" href="{{ route('blog.index') }}"> Lihat Semua Blog </a>
             </div>
